2df5129568539fd7cbd4403b7c61337b33c35999
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 08:27:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,22 +28,19 @@ class Artist
       @@all << self
   end
 
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
   def self.find_or_create_by_name(name)
-      if self.find(name)
-        self.find(name)
-      else self.create(name)
-      end
-    end
-
-    def self.create(name)
-      artist = Artist.new(name)
-      artist.save
-      artist
-    end
-
-    def self.find(name)
-      self.all.detect {|artist| artist.name == name}
-    end
+    self.find(name) || self.create(name)
+  end
 
     def print_songs
       @songs.each {|song| puts song.name}
