59811f2ac4d470863fbf211e149f8d5cb1255c85
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 06:59:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,17 +6,11 @@ class Song
     @@all << name
   end
 
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
-  
-
-  def self.all
-    @@all
+  def self.new_by_filename(filename)
+    split_filename = filename.split(" - ")
+    song = Song.new(split_filename[1])
+    song.artist = Artist.find_or_create_by_name(split_filename[0])
+    song
   end
 
 end
