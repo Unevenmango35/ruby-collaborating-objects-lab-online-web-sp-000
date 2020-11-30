9bc7cb230b7d647f5ee79c5e238a19bbe6abd075
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 07:05:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,16 @@
 class Song
   attr_accessor :name, :artist
   @@all = []
+  
   def initialize(name)
     @name = name
     @@all << self
   end
+
+  def self.all
+    @@all
+  end
+  
 
   def self.new_by_filename(filename)
     split_filename = filename.split(" - ")
