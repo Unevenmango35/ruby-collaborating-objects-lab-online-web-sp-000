88218d0939cd27ea76b003000fc6b2f50282f0c8
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 08:12:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,13 @@ class Song
       song_name = file.split(" - ")[1]
       song = Song.new(song_name)
       song.artist_name = artist_name
-     song
+     @@all << song
     end
 
 
     def artist_name=(name)
       artist = Artist.find_or_create_by_name(name)
-      self.artist = artist
-      artist.add_song(self)
+    
 end
 
     
