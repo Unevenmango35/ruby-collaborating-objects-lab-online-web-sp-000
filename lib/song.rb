acc5d62dd7e132fcc96e2018d2df7e672b3295ae
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 08:00:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,14 +13,14 @@ class Song
   end
 
 
-  def self.new_by_filename(filename)
-    song = filename.split(" - ")[1]
-    song.artist = filename.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-  #  binding.pry
-    new_song.save
-  end
+  def self.new_by_filename(file)
+      artist_name = file.split(" - ")[0]
+      song_name = file.split(" - ")[1]
+      song = Song.new(song_name)
+      song.artist_name = artist_name
+      song
+    end
+
 
   def artist_name=(name)
       self.artist = Artist.find_or_create_by_name(name)
