980ce3020c60983d1938a057a3a90dcddbbf0bb8
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 06:56:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,24 @@
 class Artist
   attr_accessor :name
   @@all = []
+
+  @@song_count = 0
   def initialize(name)
     @name = name
+    @songs = []
   end
 
 
   def self.all
     @@all
   end
+
+  def add_song(song)
+    @@song_count += 1
+    @songs << song
+    song.artist = self
+  end
+
 
   def songs
     Song.all.select {|songs| songs.artist == self}
