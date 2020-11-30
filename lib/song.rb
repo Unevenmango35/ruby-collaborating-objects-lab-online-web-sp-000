379bf700200d78165776a1aed660d5c8b4073662
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-30 07:41:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,21 +14,20 @@ class Song
 
 
   def self.new_by_filename(filename)
-    array = filename.split(/[-,]/)
-    @artist = array[0].strip
-    @song_name = array[1].strip
-    @song = Song.new(@song_name)
-    @artist = Artist.find_or_create_by_name(@artist)
-    @song.artist = @artist
-    @artist.add_song(@song)
-     #binding.pry
-    @song
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+  #  binding.pry
+    new_song.save
   end
 
   def artist_name=(name)
       self.artist = Artist.find_or_create_by_name(name)
-     artist.add_song(self)
     end
 
+    def save
+        @@all << self
+        @self
+      end
 
 end
